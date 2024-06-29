93a823c426462b91b6dd0998d3f04df3f83cabfc
LuizRossini <luiz.guirossini123@gmail.com>
2024-06-29 18:10:27 -0300

    sobrescrita de método

--- src/Principal.java
+++ src/Principal.java
@@ -21,5 +21,7 @@ public class Principal {
         lost.exibeFichaTecnica();
         lost.setTemporadas(10);
         lost.setEpisodiosPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+        System.out.println("Duração para maratonar Lost: " +lost.getDuracaoEmMinutos());
     }
 }
