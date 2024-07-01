20909f757ba8accbe82954f5b9297f09c8d09b0b
LuizRossini <luiz.guirossini123@gmail.com>
2024-07-01 19:54:29 -0300

    metodo incluido superclasse

--- src/br/com/alura/screenmatch/calculos/CalculadoraDeTempo.java
+++ src/br/com/alura/screenmatch/calculos/CalculadoraDeTempo.java
@@ -1,6 +1,5 @@
 package br.com.alura.screenmatch.calculos;
-
-import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Titulo;
 
 public class CalculadoraDeTempo {
     private int tempoTotal;
@@ -9,7 +8,16 @@ public class CalculadoraDeTempo {
         return tempoTotal;
     }
 
-    public void inclui(Filme f) {
+    /*public void inclui(Filme f) {
         tempoTotal += f.getDuracaoEmMinutos();
     }
+
+    public void inclui(Serie s) {
+        tempoTotal += s.getDuracaoEmMinutos();
+    }*/
+
+    public void inclui(Titulo titulo) {
+        this.tempoTotal += titulo.getDuracaoEmMinutos();
+    }
+
 }
