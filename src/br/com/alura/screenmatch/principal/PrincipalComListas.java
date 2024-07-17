6be18f892ff58f5d44ec10c6be743acb2d444df4
LuizRossini <luiz.guirossini123@gmail.com>
2024-07-17 18:17:18 -0300

    instancing objects in new class and refactoring ArrayList's object

--- src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -1,7 +1,19 @@
 package br.com.alura.screenmatch.principal;
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+
+import java.util.ArrayList;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
+        Filme meuFilme = new Filme("O poderoso chefão", 1970);
+        Filme outroFilme = new Filme("Avatar", 2023);
+        var filmeDoPaulo = new Filme("Dogville", 2003);
+        Serie lost = new Serie("Lost", 2000);
 
+        ArrayList<Filme> lista = new ArrayList<>();
+        lista.add(filmeDoPaulo);
+        lista.add(meuFilme);
+        lista.add(outroFilme);
     }
 }
